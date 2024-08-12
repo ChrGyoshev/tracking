c9a55b4acab640860f74dc3eb5b52788d05fccfc
ChrGyoshev <krischan@abv.bg>
2024-08-12 18:04:41 +0300

    added spinner component

--- tracking_app/src/App.jsx
+++ tracking_app/src/App.jsx
@@ -1,12 +1,12 @@
 import IntroCard from "./introCard";
 import "./App.css";
+import { useState } from "react";
+import Spinner from "./Spinner";
 
 function App() {
-  return (
-    <>
-      <IntroCard />
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  return <>{loading ? <Spinner /> : <IntroCard />}</>;
 }
 
 export default App;
---
+++ tracking_app/src/Spinner.jsx
@@ -0,0 +1,11 @@
+import Spinner from "react-bootstrap/Spinner";
+
+export default () => {
+  return (
+    <>
+      <div className="spinner">
+        <Spinner animation="border" />
+      </div>
+    </>
+  );
+};
